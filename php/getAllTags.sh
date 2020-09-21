79b87ae9220d01ae73697a757cdf2f6b9baad043
Hauke Bartsch <HaukeBartsch@gmail.com>
2020-09-21 15:51:12 +0200

    make local copy work

--- php/getAllTags.sh
+++ php/getAllTags.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-# pull all data from IDS for this project and produce file lists for unique tags over time
+# parse the directory tree for DICOM files in studies
+# we will assume that we have folders in project_name that
+# correspond to studies. Parsing them we get image series and
+# one example DICOM per folder.
 if [ $# -ne 2 ]; then
     echo "Usage: <uid> <project name>"
     exit
@@ -27,20 +30,17 @@ rm "$tmp"
 # put results into this folder
 output="data/$uid/"
 
-od=$(mktemp -d -t reviewTags-XXXXXXXX)
-
 DCMDICTPATH=/usr/share/dcmtk/dicom.dic
-/usr/bin/findscu -v -aet FIONA -aec DICOM_QR_SCP --study -k "(0008,0052)=STUDY" -k "InstitutionName=$project" -k "PatientID" -k "StudyInstanceUID" -od "${od}/" -X +sr --repeat 2 vir-app5274.ihelse.net 7840
+#/usr/bin/findscu -v -aet FIONA -aec DICOM_QR_SCP --study -k "(0008,0052)=STUDY" -k "InstitutionName=$project" -k "PatientID" -k "StudyInstanceUID" -od "${od}/" -X +sr --repeat 2 vir-app5274.ihelse.net 7840
 
 # now ask for all these studies
 OLDIFS=$IFS
 IFS=$'\n'
-fileArray=($(find $od -type f))
+fileArray=($(find "/data/${project}" -type d))
 IFS=$OLDIFS
 
-
 # instead of moving all, lets check first if there is something to do for this study - check for a sequence that has the pattern
-echo "Download all DICOM files..."
+echo "Parse all ${#fileArray[@]} folders in /data/${project}..."
 od2=$(mktemp -d -t reviewTags-pp2-XXXXXXXX)
 tLen=${#fileArray[@]}
 
@@ -48,6 +48,8 @@ tmp="$(mktemp)"
 /usr/bin/jq '. |= .+ { "total_num_participants": "'"${tLen}"'" }' "$infofilename" > "$tmp"
 mv "$tmp" $infofilename
 rm "$tmp"
+
+declare -a studiesWithSeries
 
 for (( i=0; i<${tLen}; i++ ));
 do
@@ -57,48 +59,44 @@ do
     rm "$tmp"
     
     # get series for this study with sequence per series (download everything)
-    file="${fileArray[$i]}"
-    StudyInstanceUID=`dcmdump +P "StudyInstanceUID" "${file}" | cut -d'[' -f 2 | cut -d']' -f1`
-    PatientID=`dcmdump +P "PatientID" "${file}" | cut -d'[' -f 2 | cut -d']' -f1`
-    \rm -f "${od2}/${StudyInstanceUID}/*"
-    if [ ! -d "${od2}/${StudyInstanceUID}" ]; then
-	mkdir -p "${od2}/${StudyInstanceUID}"
-    fi
+    folder="${fileArray[$i]}"
+    # for each folder create a raw directory structure with folders by series instance uid and a single DICOM file each
+    OLDIFS=$IFS
+    IFS=$'\n'
+    filesPerFolder=($(find "/data/${project}/${folder}" -type f))
+    IFS=$OLDIFS
+    tLenPerFolder=${#filesPerFolder[@]}
+    for (( i=0; i < ${tLenPerFolder}; i++ )); do
+        file="${filesPerFolder[$i]}"
+        # get StudyInstanceUID and SeriesInstanceUID
+        StudyInstanceUID=`dcmdump +P "StudyInstanceUID" "${file}" | cut -d'[' -f 2 | cut -d']' -f1`
+        SeriesInstanceUID=`dcmdump +P "SeriesInstanceUID" "${file}" | cut -d'[' -f 2 | cut -d']' -f1`
+        if [ ${studiesWithSeries[$StudyInstanceUID]+abc} ]; then
+            # check if the series is in there
+            if [ ${studiesWithSeries[$StudyInstanceUID][$SeriesInstanceUID]+abc } ]; then
+                echo "series already known"
+            else
+                studiesWithSeries[$StudyInstanceUID][$SeriesInstanceUID]="/data/${project}/${folder}/${file}"
+                # one file is sufficient
+                /usr/bin/dcmdump "/data/${project}/${folder}/${file}" | grep -v "PixelData" | egrep -v "^#" | grep -v ") FD " \
+                    | grep -v ") DT " | grep -v ") DA " \
+                    | grep -v ") OD " | grep -v ") UI " | grep -v ") US " \
+                    | grep -v ") UL " | grep -v ") SL " | grep -v ") TM " | grep -v ") UN " \
+                    | grep -v "1 WindowCenter" | grep -v "1 WindowWidth" | grep -v "1 SliceLocation" \
+                    | grep -v "3 ImagePositionPatient" \
+                    | sort | uniq > ${output}/${StudyInstanceUID}/${SeriesInstanceUID}.cache
+                # | egrep -v "[^\[]+\[[+0-9\\-\.]+\].*"
+                # store the number of files as well - does not work anymore because we are triggered at the first file here
+                # echo "(0020,1209) IS [$(ls "${path}"/* | wc -l)] # Number of series related images" >> ${output}/${StudyInstanceUID}/${SeriesInstanceUID}.cache
 
-    # we need the list of series, so movescu each one individually
-    /usr/bin/findscu -v -S -aet FIONA -aec DICOM_QR_SCP -k "(0008,0052)=SERIES" -k "StudyInstanceUID=$StudyInstanceUID" -k "SeriesInstanceUID" -od "${od2}/${StudyInstanceUID}/" -X +sr --repeat 2 vir-app5274.ihelse.net 7840
+                # we should also create an image cache for this series, it will be best if we create a mosaic
+                # to limit the number of files that need to be downloaded by the client
+                ./generateImageCache.sh ${uid} ${StudyInstanceUID} ${SeriesInstanceUID} &
 
-    echo "Start loop to get all images for this study: $StudyInstanceUID folder: ${od2}/$StudyInstanceUID"
-    # now check in each of these if we get a value (they are structured reports here, so move them to our disk)
-    series=($(find "${od2}/${StudyInstanceUID}/" -type f))
-    seriesLen=${#series[@]}
-    for (( j=0; j<${seriesLen}; j++ ));
-    do
-	ser="${series[$j]}"
-	echo "${ser} `dcmdump ${ser}`"
-        #label=`dcmdump -q  +P "0070,0006" "${ser}" | cut -d'[' -f2 | cut -d']' -f1`
-        SeriesInstanceUID=`dcmdump -q +P "SeriesInstanceUID" "${ser}" | cut -d'[' -f2 | cut -d']' -f1`
-	# get all unique tags for this series
-	path="/data/site/raw/${StudyInstanceUID}/${SeriesInstanceUID}"
-	if [ ! -d "${path}" ]; then
-            /usr/bin/movescu -v -aet FIONA -aec DICOM_QR_SCP --study -k "0008,0052=SERIES" -k "StudyInstanceUID=$StudyInstanceUID" -k "SeriesInstanceUID=$SeriesInstanceUID" -aem "FIONA_AI" vir-app5274.ihelse.net 7840
-	fi
-	echo "Get dcmdump from all files in ${path}/*"
-	mkdir -p "${output}/${StudyInstanceUID}"
-	/usr/bin/dcmdump "${path}"/* | grep -v "PixelData" | egrep -v "^#" | grep -v ") FD " \
-	    | grep -v ") DT " | grep -v ") DA " \
-	    | grep -v ") OD " | grep -v ") UI " | grep -v ") US " \
-	    | grep -v ") UL " | grep -v ") SL " | grep -v ") TM " | grep -v ") UN " \
-	    | grep -v "1 WindowCenter" | grep -v "1 WindowWidth" | grep -v "1 SliceLocation" \
-	    | grep -v "3 ImagePositionPatient" \
-	    | sort | uniq > ${output}/${StudyInstanceUID}/${SeriesInstanceUID}.cache
-	# | egrep -v "[^\[]+\[[+0-9\\-\.]+\].*"
-	# store the number of files as well
-	echo "(0020,1209) IS [$(ls "${path}"/* | wc -l)] # Number of series related images" >> ${output}/${StudyInstanceUID}/${SeriesInstanceUID}.cache
-
-	# we should also create an image cache for this series, it will be best if we create a mosaic
-	# to limit the number of files that need to be downloaded by the client
-	./generateImageCache.sh ${uid} ${StudyInstanceUID} ${SeriesInstanceUID} &	
+            fi            
+        else
+            declare -a studiesWithSeries[$StudyInstanceUID]
+        fi 
     done
 done
 
@@ -112,7 +110,7 @@ rm "$tmp"
 ./generateImageCache.sh ${uid} " " "force"
 
 # we can now make a copy of this projects data
-if [ ! -d "/var/www/html/applications/Filter/php/project_cache/${project}/" ]; then
-    mkdir -p "/var/www/html/applications/Filter/php/project_cache/${project}/"
+if [ ! -d "/var/www/html/php/project_cache/${project}/" ]; then
+    mkdir -p "/var/www/html/php/project_cache/${project}/"
 fi
-cp -R "/var/www/html/applications/Filter/php/${output}"* "/var/www/html/applications/Filter/php/project_cache/${project}/"
+cp -R "/var/www/html/php/${output}"* "/var/www/html/php/project_cache/${project}/"
