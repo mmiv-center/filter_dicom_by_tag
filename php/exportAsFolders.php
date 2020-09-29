03064429007dc570d21844b269961653b7135bfe
Hauke Bartsch <HaukeBartsch@gmail.com>
2020-09-29 14:40:22 +0200

    check if post works

--- php/exportAsFolders.php
+++ php/exportAsFolders.php
@@ -3,9 +3,12 @@
   $to_export = array();
   if (isset($_POST['to_export'])) {
       $to_export = $_POST['to_export'];
+  } else {
+      echo(json_encode(array("message" => "Error: no to_export variable found.")));
+      return;
   }
 
-  
+  echo(json_encode(array("message" => "Start doing some work.")));
 
 
 ?>
