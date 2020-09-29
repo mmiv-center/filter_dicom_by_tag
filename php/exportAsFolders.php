22e6b8debd836a427f5da2a82c3d442471b34452
Hauke Bartsch <HaukeBartsch@gmail.com>
2020-09-29 15:02:48 +0200

    call external script to do the real work

--- php/exportAsFolders.php
+++ php/exportAsFolders.php
@@ -2,13 +2,17 @@
   // Where do we store the output?
   $to_export = array();
   if (isset($_POST['to_export'])) {
-      $to_export = json_decode($_POST['to_export']);
+      $to_export = json_decode($_POST['to_export'], TRUE);
   } else {
       echo(json_encode(array("message" => "Error: no to_export variable found.")));
       return;
   }
 
-  echo(json_encode(array("message" => "Start doing some work.")));
-
+  $key = uniqid("export_");
+  $path = "/var/www/html/php/exports/".$key;
+  mkdir($path, 0777, TRUE);
+  file_put_contents($path."job.json", json_encode($to_export));
+  shell_exec("/usr/bin/nohup /var/www/html/php/run_export.sh ".$path." &");
+  echo(json_encode(array("message" => "Create export structure with key ".$key. ". Running in the background...")));
 
 ?>
