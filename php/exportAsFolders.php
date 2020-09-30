3826d15ac4c80b0294d7337c00f94be40bc39043
Hauke Bartsch <HaukeBartsch@gmail.com>
2020-09-30 15:17:11 +0200

    work in progress for export of classified data

--- php/exportAsFolders.php
+++ php/exportAsFolders.php
@@ -1,18 +1,20 @@
 <?php
   // Where do we store the output?
-  $to_export = array();
+  $key = uniqid("export_");
+  $path = "/var/www/html/php/exports/".$key;
+
+  $to_export = array( "key" => $key, "output" => $path );
   if (isset($_POST['to_export'])) {
-      $to_export = json_decode($_POST['to_export'], TRUE);
+      $to_export["data"] = json_decode($_POST['to_export'], TRUE);
   } else {
       echo(json_encode(array("message" => "Error: no to_export variable found.")));
       return;
   }
 
-  $key = uniqid("export_");
-  $path = "/var/www/html/php/exports/".$key;
   mkdir($path, 0777, TRUE);
-  file_put_contents($path."job.json", json_encode($to_export));
-  shell_exec("/usr/bin/nohup /var/www/html/php/run_export.sh ".$path." &");
-  echo(json_encode(array("message" => "Create export structure with key ".$key. ". Running in the background...")));
+  $content = $path."job.json";
+  file_put_contents($content, json_encode($to_export));
+  shell_exec("/usr/bin/nohup /var/www/html/php/run_export.sh ".$content." &");
+  echo(json_encode(array("message" => "Create export structure with key ".$key. ". Running in the background...", "key" => $key)));
 
 ?>
---
+++ php/run_export.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+#
+# export data (batch processing)
+#
+if [ $? -neq 2 ]; then
+    echo "Usage: <project folder>"
+    exit;
+fi
+
+folder="$1"
+if [ ! -e "${folder}" ]; then
+   echo "Error: folder ${folder} does not exist, or structure file not found."
+   exit;
+fi
+
+output=`jq -r ".output" "${folder}"`
+echo "OK, we are ready to start processing on the information in ${folder}."
+
+# lets start with creating the folder structure
+# each .data[i] has a key which is the StudyInstanceUID and a value which is a list of
+# SeriesInstanceUIDs
+
+for row in $(jq -r '.data[] | @base64' "${folder}"); do
+    _jq() {
+        echo ${row} | base64 --decode | jq -r ${1}
+    }
+
+   echo $(_jq '.key')
+done
