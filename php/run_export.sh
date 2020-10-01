cac4213198725ac02c2a4ecac80fc45425af2cc1
Hauke Bartsch <HaukeBartsch@gmail.com>
2020-10-01 15:41:34 +0200

    loop working with multiple series

--- php/run_export.sh
+++ php/run_export.sh
@@ -21,10 +21,17 @@ echo "OK, we are ready to start processing on the information in ${folder}."
 # each .data[i] has a key which is the StudyInstanceUID and a value which is a list of
 # SeriesInstanceUIDs
 
-for row in $(jq -r '.data[] | @base64' "${folder}"); do
+for row in $(jq -r '.data | @base64' "${folder}"); do
     _jq() {
         echo ${row} | base64 --decode | jq -r ${1}
     }
-    
-   echo $(_jq 'keys')
+
+    #echo $(_jq 'keys' | jq ".[]")                                                                                                                                                                                                                                                  
+    for u in $(_jq 'keys' | jq ".[]"); do
+        echo "work on this study: ${u}"
+        for v in $(_jq ".[${u}][]"); do
+            echo "  series: ${v}"
+	    # ok, now where is the data?
+        done
+    done
 done
